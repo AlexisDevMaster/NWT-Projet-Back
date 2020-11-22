0b1956552c7279af1a37308cfeb69ce37d1c3405
TheLuckyDev <theluckydev@gmail.com>
2020-11-22 05:25:33 +0100

    Add init mongo script for categories

---
+++ scripts/init.mongo_category.js
@@ -0,0 +1,46 @@
+/**
+ * This script is to insert initial data inside the collection category of the database nwt
+ * You can use it with mongo-shell or a tool like Robo3T
+ */
+
+// Insert category array
+db.getCollection('category').insertMany([
+    {
+        "title": "Gaming",
+        "thumbnail" : "",
+        "url": "gaming"
+    },
+    {
+        "title": "Vlog",
+        "thumbnail" : "",
+        "url": "vlog"
+    },
+    {
+        "title": "Music",
+        "thumbnail" : "",
+        "url": "music"
+    },
+    {
+        "title": "Animals",
+        "thumbnail" : "",
+        "url": "animals"
+    },
+    {
+        "title": "Sport",
+        "thumbnail" : "",
+        "url": "sport"
+    },
+    {
+        "title": "Entertainment",
+        "thumbnail" : "",
+        "url": "entertainment"
+    },
+    {
+        "title": "Science & Technology",
+        "thumbnail" : "",
+        "url": "science-technology"
+    },
+]);
+
+// display the final initial data
+db.getCollection('category').find({});
