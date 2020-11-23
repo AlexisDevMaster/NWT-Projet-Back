27603f0badd10108c09a0de92bfd34327d7eb83c
robocopv6 <redsirop@gmail.com>
2020-11-23 23:43:33 +0100

    add controller

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,6 +6,8 @@ import { AuthService } from './auth.service';
 import { jwtConstants } from './constants';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
+import { AuthController } from './auth.controller';
+
 
 @Module({
   imports: [
@@ -16,6 +18,7 @@ import { LocalStrategy } from './strategies/local.strategy';
       signOptions: { expiresIn: '3d' },
     }),
   ],
+  controllers: [ AuthController ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
   exports: [AuthService],
 })
